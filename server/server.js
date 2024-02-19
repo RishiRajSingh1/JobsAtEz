87bbfca1e7e2d7efc25a2c3f3f8d17ab144ab74f
Rishi Raj Singh <78776240+RishiRajSingh1@users.noreply.github.com>
2024-02-19 17:07:23 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -23,12 +23,6 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoute);
-// app.use("/api/users", userRoute);
-// app.use("/api/gigs", gigRoute);
-// app.use("/api/orders", orderRoute);
-// app.use("/api/conversations", conversationRoute);
-// app.use("/api/messages", messageRoute);
-// app.use("/api/reviews", reviewRoute);
 
 app.use((err, req, res, next) => {
   const errorStatus = err.status || 500;
