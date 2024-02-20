a1e7e4e5b842467fdb865f87557c7503c24db398
Rishi Raj Singh <rishisinghrajputr2@gmail.com>
2024-02-20 22:39:15 +0530

    Message and Conversation Commit

--- server/routes/review.route.js
+++ server/routes/review.route.js
@@ -7,7 +7,6 @@ import {
 } from "../controllers/review.controller.js";
 
 const router = express.Router();
-
 router.post("/", verifyToken, createReview )
 router.get("/:gigId", getReviews )
 router.delete("/:id", deleteReview)
--- server/server.js
+++ server/server.js
@@ -4,8 +4,8 @@ import dotenv from "dotenv";
 import userRoute from "./routes/user.route.js";
 import gigRoute from "./routes/gig.route.js";
 import orderRoute from "./routes/order.route.js";
-// import conversationRoute from "./routes/conversation.route.js";
-// import messageRoute from "./routes/message.route.js";
+import conversationRoute from "./routes/conversation.route.js";
+import messageRoute from "./routes/message.route.js";
 import reviewRoute from "./routes/review.route.js";
 import authRoute from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
@@ -32,8 +32,8 @@ app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/gigs", gigRoute);
 app.use("/api/orders", orderRoute);
-// app.use("/api/conversations", conversationRoute);
-// app.use("/api/messages", messageRoute);
+app.use("/api/conversations", conversationRoute);
+app.use("/api/messages", messageRoute);
 app.use("/api/reviews", reviewRoute);
 
 
