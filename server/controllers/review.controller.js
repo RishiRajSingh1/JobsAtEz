ccab16f1803d2d24baa8c0b107955372459e6643
Rishi Raj Singh <rishisinghrajputr2@gmail.com>
2024-02-21 10:31:54 +0530

    Updated delete in Review Controller

--- server/controllers/review.controller.js
+++ server/controllers/review.controller.js
@@ -47,6 +47,11 @@ export const getReviews = async (req, res, next) => {
 };
 export const deleteReview = async (req, res, next) => {
   try {
+    const review = await Review.findById(req.params.id);
+    if (review.userId !== req.userId)
+      return next(createError(403, "You can delete only your review!"));
+    await Review.findByIdAndDelete(req.params.id);
+    res.status(200).send("Review has been deleted!");
   } catch (err) {
     next(err);
   }
