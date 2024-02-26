53c9b61d27462334a80c89917a15005656a2109f
Rishi Raj Singh <rishisinghrajputr2@gmail.com>
2024-02-26 11:35:33 +0530

    Frontend Home UI Commit

---
+++ JobsAtEz/src/components/CategoryCard/CategoryCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './CategoryCard.scss'
+
+const CategoryCard = () => {
+  return (
+    <div className="container">
+        <img src="../../../public/images/cartoonanimation.png" alt="" />
+    </div>
+  )
+}
+
+export default CategoryCard
--- JobsAtEz/src/components/Home/Home.jsx
+++ JobsAtEz/src/components/Home/Home.jsx
@@ -4,16 +4,35 @@ import './Home.scss'
 import Banner from '../Banner/Banner'
 import Options from '../Options/Options'
 import About from '../About/About'
+import CategoryCard from '../CategoryCard/CategoryCard'
 
 const Home = () => {
   return (
     <>
+    <div className="Home">
     <div className='Banner'>
         <Navbar/>
         <Banner/>
         <Options/>
     </div>
     <About/>
+    <div className="category">
+      <h1>Choose Different <span>Category</span> </h1>
+      <div className="categorycard">
+      <CategoryCard/>
+      <CategoryCard/>
+      <CategoryCard/>
+      <CategoryCard/>
+      <CategoryCard/>
+      <CategoryCard/>
+      <CategoryCard/>
+      <CategoryCard/>
+      </div>
+      
+    </div>
+    <button className='btn'>More Categories</button>
+    </div>
+    
    
    
     </>
