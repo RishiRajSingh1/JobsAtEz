825163b355a3c57c3000ff493613f8647374428a
Rishi Raj Singh <rishisinghrajputr2@gmail.com>
2024-04-13 21:52:39 +0530

    Made changes in server.js

--- server/server.js
+++ server/server.js
@@ -26,7 +26,7 @@ const connect = async () => {
   }
 };
 
-app.use(cors({ credentials: true }));
+app.use(cors({ origin: `http://localhost:${process.env.FRONTEND_PORT}`||"*", credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
