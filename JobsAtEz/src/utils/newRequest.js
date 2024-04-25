2a57343a0fcdd61cd0a1289f79abd4b74ca0d1b3
Rishi Raj Singh <rishisinghrajputr2@gmail.com>
2024-04-25 15:58:37 +0530

    Frontend Utilities Update

--- JobsAtEz/src/utils/newRequest.js
+++ JobsAtEz/src/utils/newRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const newRequest = axios.create({
-  baseURL: "http://localhost:4000/api/",
+  baseURL: "https://jobsatez.onrender.com/api/",
   withCredentials: true,
 });
 
