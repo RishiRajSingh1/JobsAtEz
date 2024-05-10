5d13a34e2eaccd932bf54bee097917768b036ed0
Rishi Raj Singh <rishisinghrajputr2@gmail.com>
2024-05-10 10:34:12 +0530

    Base Url Update

--- JobsAtEz/src/utils/newRequest.js
+++ JobsAtEz/src/utils/newRequest.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const newRequest = axios.create({
-  // baseURL: "https://jobsatez.onrender.com/api/",
-  baseURL: "http://localhost:4000/api/",
+  baseURL: "https://jobsatez.onrender.com/api/",
+  // baseURL: "http://localhost:4000/api/",
   withCredentials: true,
 });
 
